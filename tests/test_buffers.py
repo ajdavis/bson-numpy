289ae78e9ace54afd9f1ad96cce595289b44cd43
A. Jesse Jiryu Davis <jesse@mongodb.com>
2016-05-29 10:47:16 -0700

    Refactor test_buffers.py.

--- tests/test_buffers.py
+++ tests/test_buffers.py
@@ -1,12 +1,28 @@
-import unittest
 from ctypes import *
 
 import numpy as np
-import numpy.testing as nptest
 from numpy import ma
 
 import bson_numpy as bn
 from bson_numpy import libbson
+
+from tests import BSONNumPyTestBase
+
+doc0 = libbson.bson_new()
+libbson.bson_append_int32(doc0, 'a', -1, 1)
+libbson.bson_append_int32(doc0, 'b', -1, 2)
+
+# Lacks 'a', adds 'c'.
+doc1 = libbson.bson_new()
+libbson.bson_append_int32(doc1, 'b', -1, 3)
+libbson.bson_append_int32(doc1, 'c', -1, 4)
+docs = [doc0, doc1]
+dtype = [('a', np.int32), ('b', np.int32)]
+
+# The '1' in the mask marks a missing value.
+expected_array = ma.array([(1, 2), (0, 3)],
+                          mask=[(0, 0), (1, 0)],
+                          dtype=dtype)
 
 
 def create_buffer(docs):
@@ -25,39 +41,18 @@ def create_buffer(docs):
     return buf, buf_len
 
 
-class BSONNumPyTest(unittest.TestCase):
-    def setUp(self):
-        doc0 = libbson.bson_new()
-        libbson.bson_append_int32(doc0, 'a', -1, 1)
-        libbson.bson_append_int32(doc0, 'b', -1, 2)
-
-        # Lacks 'a', adds 'c'.
-        doc1 = libbson.bson_new()
-        libbson.bson_append_int32(doc1, 'b', -1, 3)
-        libbson.bson_append_int32(doc1, 'c', -1, 4)
-        self.docs = [doc0, doc1]
-        self.dtype = [('a', np.int32), ('b', np.int32)]
-
-        # The '1' in the mask marks a missing value.
-        self.expected_array = ma.array([(1, 2), (0, 3)],
-                                       mask=[(0, 0), (1, 0)],
-                                       dtype=self.dtype)
-
-    def assert_array_eq(self, actual, expected, err_msg=''):
-        nptest.assert_array_equal(actual, expected, err_msg=err_msg)
-        self.assertEqual(actual.dtype, expected.dtype)
-
+class TestBuffers(BSONNumPyTestBase):
     def test_bson_buffer(self):
         buf, buf_len = create_buffer(self.docs)
-        self.assert_array_eq(bn.from_bson_buffer(buf, buf_len, self.dtype),
-                             self.expected_array)
+        self.assert_array_eq(bn.from_bson_buffer(buf, buf_len, dtype),
+                             expected_array)
 
     def test_bson_array(self):
         bson_array = libbson.bson_new()
-        for i, doc in enumerate(self.docs):
+        for i, doc in enumerate(docs):
             assert libbson.bson_append_document(bson_array, str(i), -1, doc)
 
         buf = libbson.bson_get_data(bson_array)
         self.assert_array_eq(
-            bn.from_bson_array(buf, bson_array.contents.len, self.dtype),
-            self.expected_array)
+            bn.from_bson_array(buf, bson_array.contents.len, dtype),
+            expected_array)
